ebe522053ffd3f7a0561434b1107dbc97425fed4
abbasalshafai <abbas.alshafai@gmail.com>
2022-03-31 09:03:22 -0400

    added repo constants

---
+++ lib/constants/repo_constants.dart
@@ -0,0 +1,9 @@
+// Version:	0.4
+// Date:	2021-07-24T14:54:27.882
+
+class BuzzRepoConstants{
+  static const String id = 'id';
+  static const String localId = 'lid';
+  static const String cacheId = 'cid';
+  static const String uid = 'uid';
+}
