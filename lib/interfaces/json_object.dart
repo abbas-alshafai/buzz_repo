e7cf11b40456ee7dcd87630807f7c7e81fa9f7f2
abbasalshafai <abbas.alshafai@gmail.com>
2022-04-05 20:10:35 -0400

    refactored json objects

--- lib/interfaces/json_object.dart
+++ lib/interfaces/json_object.dart
@@ -1,8 +1,9 @@
 
+import 'package:buzz_repo/interfaces/to_json_object.dart';
+
 import '../models/ids.dart';
 
-abstract class JsonObject<T> {
-  Map<String, Object?> toJson();
+abstract class JsonObject<T> extends ToJsonObject{
 
   IDs get ids;
   T copyWithIDs(IDs ids);
--- lib/interfaces/to_json_object.dart
+++ lib/interfaces/to_json_object.dart
@@ -2,5 +2,4 @@ import 'package:buzz_repo/utils/json_utils.dart';
 
 abstract class ToJsonObject<T>{
   Map<String, Object?> toJson();
-  String get toEncoded => JsonUtils().toEncodedJson(this);
 }
